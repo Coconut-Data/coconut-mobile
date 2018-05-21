5cbe40b35a2071f10f595d651247f9ab4d644141
mikeymckay <mikeymckay@gmail.com>
2018-05-21 15:45:05 +0300

    Changed internet check to use PouchDB's info() instead of plain ajax

--- _attachments/bundleCss.sh
+++ _attachments/bundleCss.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat css/fonts.css css/materialdesignicons.min.css css/material.css css/override_mdl.css css/styles.css css/modal-dialog.css css/jquery.dataTables.min.css | ./node_modules/uglifycss/uglifycss > css/bundle-css.min.css
+cat css/fonts.css css/materialdesignicons.min.css css/material.css css/override_mdl.css css/styles.css css/modal-dialog.css css/jquery.dataTables.min.css css/datatables.min.css | ./node_modules/uglifycss/uglifycss > css/bundle-css.min.css
--- _attachments/bundleJsLibraries.sh
+++ _attachments/bundleJsLibraries.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cat ./js-libraries/material.min.js ./js-libraries/mdl-select.js ./js-libraries/coffee-script.js  | ./node_modules/uglify-js/bin/uglifyjs > bundle-libraries.min.js
+cat ./js-libraries/material.min.js ./js-libraries/mdl-select.js ./js-libraries/coffee-script.js ./js-libraries/datatables.min.js | ./node_modules/uglify-js/bin/uglifyjs > bundle-libraries.min.js
 
