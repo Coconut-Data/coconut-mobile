6d2ee59fd9692e4338a17c92851281be56acbd70
esurface <esurface@rti.org>
2022-08-22 17:13:28 -0400

    set up server install

--- _attachments/deploy.sh
+++ _attachments/deploy.sh
@@ -6,15 +6,16 @@ COMMIT=$(git rev-parse --short HEAD)
 sed -i "s/ - .*<\/div>/ - <a href='https:\/\/github.com\/ICTatRTI\/coconut-mobile\/commit\/$COMMIT'>$COMMIT<\/a><\/div>/" app/views/MenuView.coffee
 sed -i "s/ - .*<\/div>/ - <a href='https:\/\/github.com\/ICTatRTI\/coconut-mobile\/commit\/$COMMIT'>$COMMIT<\/a><\/div>/" app/views/LoginView.coffee
 echo "Browserifying, uglifying and then making bundle.js"
-./node_modules/browserify/bin/cmd.js --verbose -t coffeeify --extension='.coffee' app/start.coffee -x moment -x jquery -x backbone -x pouchdb-core -x pouchdb-adapter-http -x pouchdb-find -x pouchdb-mapreduce -x pouchdb-replication -x pouchdb-upsert -x underscore -x tabulator-tables | npx terser > bundle.js
+./node_modules/browserify/bin/cmd.js --verbose -t coffeeify --extension='.coffee' app/start.coffee -x moment -x jquery -x backbone -x pouchdb-core -x pouchdb-adapter-http -x pouchdb-find -x pouchdb-mapreduce -x pouchdb-replication -x pouchdb-upsert -x underscore -x tabulator-tables > bundle.js
 ls -al bundle.js
 
 echo "Minifying bundle-css.min.css"
 ./bundleCss.sh
 echo "Minifying bundle-libraries.min.js"
 ./bundleJsLibraries.sh
-workbox generateSW workbox-config.js
-echo "Rsyncing to cloud"
-rsync --verbose  --copy-links --recursive --exclude=node_modules ./ mobile.cococloud.co:/var/www/mobile.cococloud.co/ | grep total
+
+#workbox generateSW workbox-config.js
+#echo "Rsyncing to cloud"
+#rsync --verbose  --copy-links --recursive --exclude=node_modules ./ mobile.cococloud.co:/var/www/mobile.cococloud.co/ | grep total
 # No longer use couch for serving the app so this isn't required
 #couchapp push --no-atomic cococloud  
